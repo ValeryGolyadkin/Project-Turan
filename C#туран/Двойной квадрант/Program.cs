250fc9b9ad963c0058ad4daec815785b6493f68b
VELES <v.golyadkin2005@gmail.com>
2024-10-01 14:42:31 +0600

    Hello Turan

---
+++ C#туран/Двойной квадрант/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class Program
+{
+    static (double, double) CalculateRoots(double t)
+    {
+        if (t < 0) throw new ArgumentException($"Ошибка: Значение t = {t:F3} отрицательное, корней нет.");
+        return (Math.Sqrt(t), -Math.Sqrt(t));
+    }
+
+    static void Main()
+    {
+        try
+        {
+            double t1, t2, x1 = 0, x2 = 0, x3 = 0, x4 = 0, Dis;
+            bool validRootsForT1 = false, validRootsForT2 = false;
+            Console.WriteLine("Введите значения a, b и c:");
+            double a = Convert.ToDouble(Console.ReadLine()), b = Convert.ToDouble(Console.ReadLine()), c = Convert.ToDouble(Console.ReadLine());
+            if (a == 0) throw new DivideByZeroException("Значение 'a' не должно быть равно 0.");
+            Dis = Math.Pow(b, 2) - 4 * a * c;
+            if (Dis < 0) Console.WriteLine("Дискриминант ниже 0! Корней уравнения нет.");
+            else
+            {
+                t1 = (-b + Math.Sqrt(Dis)) / (2 * a); 
+                t2 = (-b - Math.Sqrt(Dis)) / (2 * a);
+                try { (x1, x2) = CalculateRoots(t1); validRootsForT1 = true; Console.WriteLine($"Корни для t1: {x1:F3}, {x2:F3}"); }
+                catch (ArgumentException ex) { Console.WriteLine(ex.Message); }
+                try { (x3, x4) = CalculateRoots(t2); validRootsForT2 = true; Console.WriteLine($"Корни для t2: {x3:F3}, {x4:F3}"); }
+                catch (ArgumentException ex) { Console.WriteLine(ex.Message); }
+                if (validRootsForT1 && validRootsForT2 && (x1 == x3 && x2 == x4))
+                    Console.WriteLine($"Корни двойного квадратного уравнения: {x1}, {x2}");
+                else
+                {
+                    if (validRootsForT1) Console.WriteLine($"Действительные корни для t1: {x1:F3}, {x2:F3}");
+                    if (validRootsForT2) Console.WriteLine($"Действительные корни для t2: {x3:F3}, {x4:F3}");
+                }
+            }
+        }
+        catch (FormatException) { Console.WriteLine("Ошибка: Введено некорректное число."); }
+        catch (DivideByZeroException ex) { Console.WriteLine($"Ошибка: {ex.Message}"); }
+        catch (Exception ex) { Console.WriteLine($"Произошла ошибка: {ex.Message}"); }
+    }
+}
+
---
+++ C#туран/Треугольники/Program.cs
@@ -0,0 +1,23 @@
+﻿double SideA = Convert.ToDouble(Console.ReadLine());
+double SideB = Convert.ToDouble(Console.ReadLine());
+double SideС = Convert.ToDouble(Console.ReadLine());
+double halfPer = (SideA + SideB + SideС) / 2;
+double area = Math.Sqrt(halfPer * (halfPer - SideA) * (halfPer - SideB) *
+    (halfPer - SideС));
+Type areatype = area.GetType();
+if (area > 0)
+{
+    Console.WriteLine("Triangle area with sides {0}, {1}, {2}, is {3}",
+    SideA, SideB, SideС, area);
+}
+else Console.WriteLine("Triangle area with sides {0}, {1}, {2}, is incorrect",
+    SideA, SideB, SideС);
+
+if (Math.Pow(SideС, 2) + Math.Pow(SideA, 2) == Math.Pow(SideB, 2) ||
+    Math.Pow(SideA, 2) + Math.Pow(SideB, 2) == Math.Pow(SideС, 2) ||
+    Math.Pow(SideС, 2) + Math.Pow(SideB, 2) == Math.Pow(SideA, 2))
+{
+    Console.WriteLine("Triangle is right-angled");
+}
+else Console.WriteLine("Triangle not right-angled");
+
